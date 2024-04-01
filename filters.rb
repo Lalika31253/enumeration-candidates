c361a758c951c024f4e6d8c35f8ee0f96a66bbf0
Lalika31253 <Lalika31253@gmail.com>
2024-04-01 14:27:10 -0600

    Define find_by_id, experienced?, qualified_candidates and ordered_by_qualifications methods

--- filters.rb
+++ filters.rb
@@ -2,15 +2,34 @@
 # This way, we keep these methods separated from other potential parts of the program
 
 def find(id)
-    # Your code Here
+  @candidates.find { |candidate| candidate[:id] == id }
   end
+
   
   def experienced?(candidate)
-    # Your code Here
+    # if experienced > 2
+    #   return true
+    # else
+    #     return false
+    candidate[:years_of_experience] >= 2
   end
+
   
   def qualified_candidates(candidates)
-    # Your code Here
+    candidates.filter do |candidate|
+      #using experienced method to check if candidate has anough experience (returns true/false)
+      experienced?(candidate) &&
+      candidate[:github_points] >= 100 &&
+      candidate[:languages].include?("Ruby") || candidate[:languages].include?("Python") &&
+      candidate[:date_applied] >= 15.days.ago.to_date &&
+      candidate[:age] > 17
+    end
+    
   end
   
-  # More methods will go below
+
+  def ordered_by_qualifications(candidates)
+    # - sorting in descending order
+    candidates.sort_by { |candidate| [-candidate[:years_of_experience], -candidate[:github_points]] }
+  end
+
